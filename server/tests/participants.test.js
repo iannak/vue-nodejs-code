fb38d9444577602f423b263a3f25c457c0533d0a
franzanna <franzanna@pop-os.localdomain>
2023-05-30 09:40:07 -0300

    refactor tests

--- server/tests/participants.test.js
+++ server/tests/participants.test.js
@@ -1,45 +1,144 @@
-// tests/participants.test.js
 const request = require("supertest");
-const app = require("../src/app");
+const app = require("../app");
 const mongoose = require("mongoose");
 const Participant = require("../src/models/participant");
 
 describe("Participants API", () => {
-    beforeAll(async () => {
-        await mongoose.connect("mongodb://localhost:27017/testdb", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+  beforeAll(async () => {
+    await mongoose.connect("mongodb://localhost:27017/testdb", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }, 30000);
+
+  afterEach(async () => {
+    await Participant.deleteMany();
+  }, 30000);
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  }, 30000);
+
+  it("should create a new participant", async () => {
+    const response = await request(app).post("/participants").send({
+      firstName: "John",
+      lastName: "Doe",
+      percentage: 50,
     });
 
-    afterEach(async () => {
-        await Participant.deleteMany();
+    expect(response.statusCode).toBe(201);
+    expect(response.body.firstName).toBe("John");
+    expect(response.body.lastName).toBe("Doe");
+    expect(response.body.percentage).toBe(50);
+  }, 30000);
+
+  it("should get all participants", async () => {
+    await Participant.create({
+      firstName: "John",
+      lastName: "Doe",
+      percentage: 50,
+    });
+    await Participant.create({
+      firstName: "Jane",
+      lastName: "Smith",
+      percentage: 30,
     });
 
-    afterAll(async () => {
-        await mongoose.connection.close();
+    const response = await request(app).get("/participants");
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBe(2);
+    expect(response.body[0].firstName).toBe("John");
+    expect(response.body[0].lastName).toBe("Doe");
+    expect(response.body[0].percentage).toBe(50);
+    expect(response.body[1].firstName).toBe("Jane");
+    expect(response.body[1].lastName).toBe("Smith");
+    expect(response.body[1].percentage).toBe(30);
+  }, 30000);
+
+  it("should get a participant by ID", async () => {
+    const participant = await Participant.create({
+      firstName: "John",
+      lastName: "Doe",
+      percentage: 50,
     });
 
-    it("should create a new participant", async () => {
-        const response = await request(app).post("/participants").send({
-            name: "John Doe",
-            percentage: 50,
-        });
+    const response = await request(app).get(`/participants/${participant._id}`);
 
-        expect(response.statusCode).toBe(201);
-        expect(response.body.name).toBe("John Doe");
-        expect(response.body.percentage).toBe(50);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.firstName).toBe("John");
+    expect(response.body.lastName).toBe("Doe");
+    expect(response.body.percentage).toBe(50);
+  }, 30000);
+
+  it("should handle not found error when getting a participant by ID", async () => {
+    const invalidId = "invalid-id";
+
+    const response = await request(app).get(`/participants/${invalidId}`);
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body.error).toBe("Participante não encontrado");
+  }, 30000);
+
+  it("should update a participant", async () => {
+    const participant = await Participant.create({
+      firstName: "John",
+      lastName: "Doe",
+      percentage: 50,
     });
 
-    it("should get all participants", async () => {
-        await Participant.create({ name: "John Doe", percentage: 50 });
-        await Participant.create({ name: "Jane Smith", percentage: 30 });
+    const updatedParticipant = {
+      firstName: "Jane",
+      lastName: "Smith",
+      percentage: 70,
+    };
 
-        const response = await request(app).get("/participants");
+    const response = await request(app)
+      .put(`/participants/${participant._id}`)
+      .send(updatedParticipant);
 
-        expect(response.statusCode).toBe(200);
-        expect(response.body.length).toBe(2);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.firstName).toBe("Jane");
+    expect(response.body.lastName).toBe("Smith");
+    expect(response.body.percentage).toBe(70);
+  }, 30000);
+
+  it("should handle not found error when updating a participant", async () => {
+    const invalidId = "invalid-id";
+
+    const response = await request(app)
+      .put(`/participants/${invalidId}`)
+      .send({
+        firstName: "Jane",
+        lastName: "Smith",
+        percentage: 70,
+      });
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body.error).toBe("Participante não encontrado");
+  }, 30000);
+
+  it("should delete a participant", async () => {
+    const participant = await Participant.create({
+      firstName: "John",
+      lastName: "Doe",
+      percentage: 50,
     });
 
-    // Adicione outros testes para os demais endpoints e casos de erro
+    const response = await request(app).delete(`/participants/${participant._id}`);
+
+    expect(response.statusCode).toBe(204);
+
+    const deletedParticipant = await Participant.findById(participant._id);
+    expect(deletedParticipant).toBeNull();
+  }, 30000);
+
+  it("should handle not found error when deleting a participant", async () => {
+    const invalidId = "invalid-id";
+
+    const response = await request(app).delete(`/participants/${invalidId}`);
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body.error).toBe("Participante não encontrado");
+  }, 30000);
 });
